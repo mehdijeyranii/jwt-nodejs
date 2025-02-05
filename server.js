fcea6f9259710fc0065bba6d0a9c73b2c728d843
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-02-05 18:38:30 +0330

    Creating a JWT

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/login", (req, res) => {
+  // Mock user
+  const user = { id: 1, username: "john" };
+
+  // Create a token
+  const token = jwt.sign({ user }, "your-secret-key", { expiresIn: "1h" });
+  res.json({ token });
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
